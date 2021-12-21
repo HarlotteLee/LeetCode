89c0aabdfbdd12f9f54680f8a0b09c4fc6063b50
HarlotteLee <90295070+HarlotteLee@users.noreply.github.com>
2021-12-21 19:28:41 +0800

    Update TwoSum.cpp

--- TwoSum/TwoSum.cpp
+++ TwoSum/TwoSum.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int len = nums.size();
-        vector<int> result(2);
-        int flag = 0;
-        for(int i =0; i< len -1; i++)
+        for(int i=0;i<nums.size()-1;i++)
         {
-            for(int j = i+1; j< len; j++)
+            for(int j=i+1;j<nums.size();j++)
             {
-                if(nums[i] + nums[j] == target)
+                if(nums[i]+nums[j]==target)
                 {
-                    result[0] = i+1;
-                    result[1] = j+1;
-                    flag = 1;
-                    break;
+                    return {i,j};
                 }
             }
-            
-            if(flag == 1)
-                break;
-                
         }
-        
-        return result;
-    }
+        return {};
+    };
 };
